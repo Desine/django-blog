dacdb93d65fc2b86cbe0b66abf7f83ed09e56344
Napolov Vlad <85844885+Desine@users.noreply.github.com>
2024-11-28 12:36:17 +0300

    added base_layout.html

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    date_added = models.DateField(auto_now_add=True)
+    image = models.ImageField(default='default.png', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:20].strip()
---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('post_detail/<int:pk>', post_detail, name='post_detail'),
+    path('post_create/', post_create, name='post_create')
+]
